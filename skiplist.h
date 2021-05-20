3eed9ab960c78c1a893f98969795a8fbb28bf59c
pres1017 <pres1017@gmail.com>
2021-05-20 00:11:53 -0500

    Add files via upload

---
+++ skiplist.h
@@ -0,0 +1,262 @@
+#ifndef SKIPLIST_H
+#define SKIPLIST_H
+
+#include <cstring> // for memset
+#include <cmath> // for std::size_t
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <time.h>
+#include <iostream>
+
+using std::size_t;
+using std::vector;
+
+template <typename Comparable>
+class SkipList{
+    private:
+        size_t maxLevel;
+        float fraction; // max level can only
+        size_t size; // keeps track of the elements at all levels
+
+        struct Node{
+            Comparable data; 
+            vector<Node*> next; // each level represents next node. vector size will always be size of maxLevel
+            size_t level; // indicates its position in the hierarchy (i.e. incldued in vector<level>)
+
+            Node(size_t vecSize, Comparable value = 0, size_t level=0){ // default constructor
+                this->data = value;
+                for(size_t x = 0; x < vecSize; x++){
+                    next.push_back(nullptr);
+                }
+                this->level = level;
+            }
+        };
+
+        Node* head;
+
+    public:
+        SkipList(){
+            size = 0;
+            maxLevel = 3;
+            fraction = 0.5;
+            head = new Node(maxLevel);
+            for(size_t x = 0; x < maxLevel; x++){
+                head->next[x] = nullptr; // initializing all the "levels"
+            }
+        }
+
+        explicit SkipList(size_t top, float probability=0.5){
+            maxLevel = top;
+            fraction = probability;
+            head = new Node(maxLevel);
+            size = 0;
+            for(size_t x = 0; x < maxLevel; x++){
+                head->next[x] = nullptr; // initializing all the "levels"
+            }
+        }
+
+        SkipList(const SkipList& toCopy){
+            if(this == &toCopy){
+                size = 0;
+                maxLevel = 3;
+                fraction = 0.5;
+                head = new Node(maxLevel);
+                for(size_t x = 0; x < maxLevel; x++){
+                    head->next[x] = nullptr; // initializing all the "levels"
+                }
+            }else{
+                size = 0;
+                maxLevel = toCopy.maxLevel;
+                fraction = toCopy.fraction;
+                head = new Node(maxLevel);
+                for(size_t x = 0; x < maxLevel; x++){
+                    head->next[x] = nullptr; // initializing all the "levels"
+                }
+                Node* temp = toCopy.head;
+                while(temp->next[0] != nullptr){
+                    this->insert(temp->next[0]->data);
+                    temp = temp->next[0];
+                }
+            }
+        }
+
+        SkipList& operator=(const SkipList& toCopy){
+            if(this == &toCopy){
+                size = 0;
+                maxLevel = 3;
+                fraction = 0.5;
+                head = new Node(maxLevel);
+                for(size_t x = 0; x < maxLevel; x++){
+                    head->next[x] = nullptr; // initializing all the "levels"
+                }
+            }else{
+                Node* temp1 = head->next[0];
+                while(temp1 != nullptr){
+                    Node* toDelete = temp1;
+                    temp1 = temp1->next[0];
+                    delete toDelete;
+                }
+                delete head;
+                size = 0;
+                maxLevel = toCopy.maxLevel;
+                fraction = toCopy.fraction;
+                head = new Node(maxLevel);
+                for(size_t x = 0; x < maxLevel; x++){
+                    head->next[x] = nullptr; // initializing all the "levels"
+                }
+                Node* temp2 = toCopy.head;
+                while(temp2->next[0] != nullptr){
+                    this->insert(temp2->next[0]->data);
+                    temp2 = temp2->next[0];
+                }
+            }
+            return *this;
+        }
+
+        ~SkipList(){
+            Node* temp = head->next[0];
+            while(temp != nullptr){
+                Node* toDelete = temp;
+                temp = temp->next[0];
+                delete toDelete;
+            }
+            delete head;
+        }
+
+        size_t randomLevel(){ // randomLevel function taken from references
+            float r = (float)rand()/RAND_MAX;
+            int lvl = 0;
+            while (r < fraction && lvl < maxLevel){
+                lvl++;
+                r = (float)rand()/RAND_MAX;
+            }
+            return lvl;
+        }
+
+        void printLayer(size_t x, std::ostream& os=std::cout){ // utility function used for debugging
+            Node* temp = head;
+            while(temp->next[x] != nullptr){
+                os << temp->next[x]->data << " -> ";
+                temp = temp->next[x];
+            }
+            std::cout << "tail\n";
+        }
+
+        /*
+            Explanation of insert logic:
+            curLevel starts off at the max level, and the linked list is iterated through as normal.
+            Insert operates under assumption that the list is sorted. 
+        */
+        bool insert(Comparable value){
+            Node* temp = head;
+            vector<Node*> updates;
+            for(size_t x = 0; x < maxLevel; x++){
+                updates.push_back(nullptr);
+            }
+            size_t curlevel = maxLevel - 1;
+            size_t randLevel = randomLevel();
+            if(randLevel >= maxLevel){
+                randLevel = maxLevel - 1;
+            }
+            while(curlevel > 0){ // first while loop navigates to the 0th layer skipping as many nodes as possible
+                if(temp->next[curlevel] == nullptr){updates[curlevel] = temp; curlevel--;}
+                else if(temp->next[curlevel]->data > value){updates[curlevel] = temp; curlevel--;}
+                else temp=temp->next[curlevel];
+            }
+            bool reachedEnd = false;
+            while(!reachedEnd){ // second while loop actually traverses to desired location
+                if(temp->next[curlevel] == nullptr){updates[curlevel] = temp; reachedEnd = true;}
+                else if(temp->next[curlevel]->data >= value){updates[curlevel] = temp; reachedEnd = true;}
+                else temp = temp->next[curlevel];
+            }
+            Node* toInsert = new Node(maxLevel, value, randLevel);
+
+            if(temp->next[curlevel] != nullptr){
+                if(temp->next[curlevel]->data == value){
+                    return false;
+                }
+            }
+
+            for(size_t x = 0; x < maxLevel; x++){
+                if(x <= randLevel){
+                    toInsert->next[x] = updates[x]->next[x];
+                    updates[x]->next[x] = toInsert;
+                }else{
+                    toInsert->next[x] = updates[x]->next[x];
+                }
+            }
+            size++;
+            return true;
+        }
+        
+        bool remove(Comparable value){ // remove operates very similar to insert
+            Node* temp = head;
+            vector<Node*> updates;
+            for(size_t x = 0; x < maxLevel; x++){
+                updates.push_back(nullptr);
+            }
+            size_t curlevel = maxLevel - 1;
+            while(curlevel > 0){
+                if(temp->next[curlevel] == nullptr) curlevel--;
+                else if(temp->next[curlevel]->data > value) curlevel--;
+                else if(temp->next[curlevel]->data == value) {updates[curlevel] = temp; curlevel--;}
+                else temp = temp->next[curlevel];
+            }
+            bool reachedEnd = false;
+            while(!reachedEnd){
+                if(temp->next[curlevel] == nullptr) return false;
+                else if(temp->next[curlevel]->data > value) return false;
+                else if(temp->next[curlevel]->data == value) {updates[curlevel] = temp; reachedEnd = true;}
+                else temp = temp->next[curlevel];
+            }
+            size_t theLevel = temp->next[curlevel]->level;
+            Node* toDelete = temp->next[curlevel];
+            if(theLevel == maxLevel){
+                theLevel--;
+            }
+            for(size_t x = 0; x <= theLevel; x++){
+                    updates[x]->next[x] = toDelete->next[x];
+            }
+            delete toDelete;
+            size--;
+            return true;
+        }
+        
+        size_t elements(){
+            return size;
+        }
+
+        bool contains(Comparable value){
+            Node* temp = head;
+            size_t curlevel = maxLevel -1;
+            while(curlevel > 0){
+                if(temp->next[curlevel] == nullptr) curlevel--;
+                else if(temp->next[curlevel]->data == value) return true;
+                else if(temp->next[curlevel]->data > value) curlevel--;
+                else temp = temp->next[curlevel];
+            }
+            while(temp->next[curlevel] != nullptr){
+                if (temp->next[curlevel]->data > value) return false;
+                else if(temp->next[curlevel]->data == value) return true;
+                else temp = temp->next[curlevel];
+            }
+
+            return false;
+        }
+        
+        void printElements(std::ostream& os=std::cout){
+            for(size_t x = 0; x < maxLevel; x++){
+                std::cout << "LAYER " << x << ": ";
+                this->printLayer(x, os);
+            }
+        }
+};
+
+#endif
+
+/*
+REFERENCES:
+https://www.ics.uci.edu/~pattis/ICS-23/lectures/notes/Skip%20Lists.pdf 
+https://www.geeksforgeeks.org/skip-list/
+*/
---
+++ skiptests.cpp
@@ -0,0 +1,414 @@
+#include "skiplist.h"
+#include "doubly_linked_list.h"
+#include "array_list.h"
+#include <vector>
+#include <time.h>
+
+int main(){
+    // A random number generator 
+    // Using std::clock_t for time tracking
+    // Stuck with the srand for time because it's fun, even if it is inefficient sometimes
+
+    std::cout << "Test Suite\n";
+    std::cout << "CONTAINS TEST means that the list is being checked for all the elements that were inserted. If the list contains elements it shouldn't\nor is missing elements it should have, it fails.\n";
+    std::cout << "SIZE TEST is for a counter variable. This variable keeps track of the number of elements that should be in the list, \nthen compares that to the actual number of elements in the list.\n\n";
+    { // TEST 1: Forwards and backwards
+        std::cout << "TEST 0: INSERTING 30 ELEMENTS HALF FORWARDS AND HALF BACKWARDS\n";
+        size_t count = 0;
+        SkipList<int> skpList;
+        for(int x = 0; x < 15; x++){
+            skpList.insert(x);
+            count++;
+        }
+        for(int x = 30; x >= 15; x--){
+            skpList.insert(x);
+            count++;
+        }
+        bool containsElems = true;
+        for(int x = 0; x < 30; x++){
+            if(!skpList.contains(x)) containsElems = false;
+        }
+        if(containsElems == false){
+            std::cout << "\tCONTAINS TEST: FAILED\n";
+        }else{
+            std::cout << "\tCONTAINS TEST: PASSED\n";
+        }
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+    }
+    std::cout <<"\n";
+    {
+        std::cout << "TEST 1: INSERTING 15 ODD ELEMENTS, 15 EVENS, THEN REMOVING THE ODDS\n";
+        size_t count = 0;
+        SkipList<int> skpList;
+        for(int x = 0; x < 30; x++){
+            if(x%2 == 0){
+                skpList.insert(x);
+                count++;
+            }
+        }
+
+        for(int x = 0; x < 30; x++){
+            if(x%2 != 0){
+                skpList.insert(x);
+                count++;
+            }
+        }
+
+        for(int x = 0; x < 30; x++){
+            if(x%2 != 0){
+                skpList.remove(x);
+                count--;
+            }
+        }
+
+        bool containsElems = true;
+        for(int x = 0; x < 30; x++){
+            if((x%2 != 0 && skpList.contains(x)) || (x%2 == 0 && !skpList.contains(x))) containsElems = false;
+        }
+        if(containsElems == false){
+            std::cout << "\tCONTAINS TEST: FAILED\n";
+        }else{
+            std::cout << "\tCONTAINS TEST: PASSED\n";
+        }
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 2: INSERTING AND REMOVING 100 ELEMENTS\n";
+        SkipList<int> skpList;
+        size_t count = 0;
+        for(int x = 0; x < 100; x++){
+            skpList.insert(x);
+            count++;
+        }
+
+        for(int x = 0; x < 100; x++){
+            skpList.remove(x);
+            count--;
+        }
+        bool containsElems = true;
+
+        for(int x = 0; x < 100; x++){
+            if(skpList.contains(x)) containsElems = false;
+        }
+
+        if(containsElems == false){
+            std::cout << "\tCONTAINS TEST: FAILED\n";
+        }else{
+            std::cout << "\tCONTAINS TEST: PASSED\n";
+        }
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 3: TIMING INSERTING AND REMOVING OF 10000 ELEMENTS WITH 3 LAYERS\n";
+        SkipList<int> skpList;
+        size_t count = 0;
+        long double duration1;
+        long double duration2;
+        long double duration3;
+
+        clock_t start1 = clock();
+        for(int x = -5000; x < 5000; x++){
+            skpList.insert(x);
+            count++;
+        }
+        clock_t end1 = clock();
+        duration1 = (end1 - start1) / (long double) CLOCKS_PER_SEC;
+
+        clock_t start2 = clock();
+        for(int x = 5000 - 1; x > -5000; x--){
+            skpList.remove(x);
+            count--;
+        }
+        clock_t end2 = clock();
+        duration2 = (end2 - start2) / (long double) CLOCKS_PER_SEC;
+        duration3 = duration1 + duration2;
+
+        bool containsElems = true;
+
+        for(int x = 2500; x < -2500; x++){
+            if(skpList.contains(x)) containsElems = false;
+        }
+
+        if(containsElems == false){
+            std::cout << "\tCONTAINS TEST: FAILED\n";
+        }else{
+            std::cout << "\tCONTAINS TEST: PASSED\n";
+        }
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+        std::cout << "\tTIME TO INSERT 10000 ELEMENTS IN ORDER: " << duration1 << "s\n";
+        std::cout << "\tTIME TO REMOVE 10000 ELEMENTS IN REVERSE ORDER: " << duration2 << "s\n";
+        std::cout << "\tTOTAL TIME: " << duration3 << "s\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 4: TIMING INSERTING AND REMOVING OF 10000 ELEMENTS WITH 10 LAYERS\n";
+        SkipList<int> skpList(10);
+        size_t count = 0;
+        long double duration1;
+        long double duration2;
+        long double duration3;
+
+        clock_t start1 = clock();
+        for(int x = -5000; x < 5000; x++){
+            skpList.insert(x);
+            count++;
+        }
+        clock_t end1 = clock();
+        duration1 = (end1 - start1) / (long double) CLOCKS_PER_SEC;
+
+        clock_t start2 = clock();
+        for(int x = 5000 - 1; x > -5000; x--){
+            skpList.remove(x);
+            count--;
+        }
+        clock_t end2 = clock();
+        duration2 = (end2 - start2) / (long double) CLOCKS_PER_SEC;
+        duration3 = duration1 + duration2;
+
+        bool containsElems = true;
+
+        for(int x = 2500; x < -2500; x++){
+            if(skpList.contains(x)) containsElems = false;
+        }
+
+        if(containsElems == false){
+            std::cout << "\tCONTAINS TEST: FAILED\n";
+        }else{
+            std::cout << "\tCONTAINS TEST: PASSED\n";
+        }
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+        std::cout << "\tTIME TO INSERT 10000 ELEMENTS IN ORDER: " << duration1 << "s\n";
+        std::cout << "\tTIME TO REMOVE 10000 ELEMENTS IN REVERSE ORDER: " << duration2 << "s\n";
+        std::cout << "\tTOTAL TIME: " << duration3 << "s\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 5: INSERTING 5000 ELEMENTS AND REMOVING EVERY THIRD ELMENT\n";
+        SkipList<int> skpList;
+        size_t count = 0;
+        clock_t start1 = clock();
+        for(int x = -2500; x < 2500; x++){
+            skpList.insert(x);
+            count++;
+        }
+        clock_t end1 = clock();
+        long double duration1 = (end1 - start1) / (long double) CLOCKS_PER_SEC;
+
+        clock_t start2 = clock();
+        for(int x = -2500; x < 2500; x++){
+            if(x%3 == 0){
+                skpList.remove(x);
+                count--;
+            }
+        }
+        clock_t end2 = clock();
+        long double duration2 = (end2 - start2) / (long double) CLOCKS_PER_SEC;
+        long double duration3 = duration1 + duration2;
+
+        if(count != skpList.elements()){
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }else{
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }
+        std::cout << "\tTIME TO INSERT 10000 ELEMENTS IN ORDER: " << duration1 << "s\n";
+        std::cout << "\tTIME TO REMOVE 10000 ELEMENTS IN REVERSE ORDER: " << duration2 << "s\n";
+        std::cout << "\tTOTAL TIME: " << duration3 << "s\n";
+    }
+    
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 6: INSERTING 15 DOUBLES AND PRINTING THEM\n";
+        SkipList<double> skpList;
+        for(int x = 0; x < 15; x++){
+            skpList.insert(((double)x)/3);
+        }
+        skpList.printElements();
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 7: INSERTING T VALUES AND REMOVING EVERY U ONE WITH V LAYERS (change variables in code)\n";
+        int T = 100;
+        int U = 2;
+        int V = 3;
+        std::cout << "\tT: " << T << " || U: " << U << " || V: " << V << "\n";
+        SkipList<int> skpList(V);
+        size_t count = 0;
+        clock_t start1 = clock();
+        for(int x = -T/2; x <= T/2; x++){
+            skpList.insert(x);
+            count++;
+        }
+        clock_t end1 = clock();
+        long double duration1 = (end1 - start1) / (long double) CLOCKS_PER_SEC;
+        if(skpList.contains(0)){
+            std::cout << "\tCONTAINS 0: YES\n";
+        }else{
+            std::cout << "\tCONTAINS 0: NO\n";
+        }
+        clock_t start2 = clock();
+        for(int x = T/2; x >= -T/2; x--){
+            if(x%V == 0){
+                skpList.remove(x);
+                count--;
+            }
+        }
+        clock_t end2 = clock();
+        if(skpList.elements() == count){
+            std::cout << "\tSIZE TEST: PASSED\n";
+        }else{
+            std::cout << "\tSIZE TEST: FAILED\n";
+        }
+        long double duration2 = (end2 - start2) / (long double) CLOCKS_PER_SEC;
+        long double duration3 = duration1 + duration2;
+        std::cout << "\tINSERTION TIME: " << duration1 << "s\n";
+        std::cout << "\tREMOVAL TIME: " << duration2 << "s\n";
+        std::cout << "\tTOTAL TIME: " << duration3 << "s\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 8: INSERTING 5 VALUES AND USING THE COPY CONSTRUCTOR\n";
+        SkipList<int> skpList1;
+        for(int x = 0; x <= 5; x++){
+            skpList1.insert(x);
+        }
+        SkipList<int> skpList2 = skpList1;
+        bool containsSame = true;
+        for(int x = 0; x <= 5; x++){
+            if(!skpList2.contains(x)) containsSame = false;
+        }
+        if(containsSame == true) std::cout << "\tCONTAINS THE SAME ELEMENTS\n";
+        else std::cout << "\tDOES NOT CONTAIN THE SAME ELEMENTS\n";
+
+        if(skpList1.elements() == skpList2.elements()) std::cout << "\tSIZE MATCHES\n"; 
+        else std::cout << "\tSIZE DOES NOT MATCH\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 9: SETTING A SKIP LIST WITH SOME VALUES EQUAL TO ANOTHER SKIP LIST\n";
+        SkipList<int> skpList1;
+        SkipList<int> skpList2;
+        for(int x = 0; x <= 5; x++){
+            skpList1.insert(x);
+        }
+        for(int x = 6; x <= 10; x++){
+            skpList2.insert(x);
+        }
+        skpList2 = skpList1;
+        bool containsSame = true;
+        for(int x = 0; x <= 5; x++){
+            if(!skpList2.contains(x)) containsSame = false;
+        }
+        if(containsSame == true) std::cout << "\tCONTAINS THE SAME ELEMENTS\n"; 
+        else std::cout << "\tDOES NOT CONTAIN THE SAME ELEMENTS\n";
+
+        if(skpList1.elements() == skpList2.elements()) std::cout << "\tSIZE MATCHES\n"; 
+        else std::cout << "\tSIZE DOES NOT MATCH\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 10: USING A SKIPLIST COPY CONSTRUCTOR ON ITSELF AND PERFORMING INSERTIONS/REMOVALS\n";
+        SkipList<int> skpList = skpList;
+        size_t count = 0;
+        for(int x = 0; x <= 100; x++){
+            skpList.insert(x);
+            count++;
+        }
+
+        bool containsWorks = true;
+        for(int x = 0; x <= 100; x++){
+            if(!skpList.contains(x)) containsWorks = false;
+        }
+
+        std::cout << "\tPRE-REMOVAL TESTS\n";
+        if(skpList.elements() == count) std::cout << "\t\tSIZE TEST: PASSED\n";
+        else std::cout << "\t\tSIZE TEST: FAILED\n";
+        if(containsWorks == true) std::cout << "\t\tCONTAINS TEST: PASSED\n";
+        else std::cout << "\t\tCONTAINS TEST: FAILED\n";
+
+        for(int x = 0; x <= 100; x++){
+            if(x%2 == 0){
+                skpList.remove(x);
+                count--;
+            }
+        }
+
+        bool containsWorks2 = true;
+        for(int x = 0; x <= 100; x++){
+            if(x%2 == 0 && skpList.contains(x)) containsWorks2 = false;
+        }
+        std::cout << "\tPOST-REMOVAL TESTS\n";
+        if(skpList.elements() == count) std::cout << "\t\tSIZE TEST: PASSED\n";
+        else std::cout << "\t\tSIZE TEST: FAILED\n";
+        if(containsWorks2 == true) std::cout << "\t\tCONTAINS TEST: PASSED\n";
+        else std::cout << "\t\tCONTAINS TEST: FAILED\n";
+    }
+
+    std::cout << "\n";
+
+    {
+        std::cout << "TEST 11: INSERTING MANY NUMBERS IN REVERSE ORDER\n";
+        // Consistent best-time insertions and removes, as they all happen at the front
+        int T = 10000;
+        SkipList<int> skpList;
+        clock_t start1 = clock();
+        for(int x = T; x >= 0; x--){
+            skpList.insert(x);
+        }
+        clock_t end1 = clock();
+        long double duration1 = (end1 - start1) / (long double) CLOCKS_PER_SEC;
+
+        clock_t start2 = clock();
+        for(int x = 0; x < T; x++){
+            skpList.remove(x);
+        }
+        clock_t end2 = clock();
+        long double duration2 = (end2 - start2) / (long double) CLOCKS_PER_SEC;
+
+        std::cout << "\tBEST CASE INSERTION FOR: " << T << " VALUES: " << duration1 << "\n";
+        std::cout << "\tBEST CASE REMOVAL FOR: " << T << " VALUES: " << duration2 << "\n";
+    }
+}
